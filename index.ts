9cb81992c183237e6fc7ef0dc2a502e0b72943a8
mgerashchenko <mgerashchenko@gmail.com>
2019-12-01 23:36:34 -0500

    Finish with pipe

--- index.ts
+++ index.ts
@@ -1,31 +1,116 @@
 // Import stylesheets
 import './style.css';
-import * as UTILS from './utils'
+import * as UTILS from './utils';
 
-// composeF
+
+// compose Continious Passing style
 const add1 = x => x + 1;
-const mul3 = y => y * 3; 
-
-const composeF = (f, g) => x => g(f(x));
-
-const add1ThenMul3 = composeF(add1, mul3)
+const mul3 = y => y * 3;
+const composeCPS =  (f, g) => x => g(f(x));
+const add1ThenMul3 = composeCPS(add1, mul3);
 UTILS.showResult('add1ThenMul3', add1ThenMul3(4));
 
-// async add callback
 
-// Hight Order async
-const asyncAdd1 = (x, cb) => {
-  setTimeout( () => { 
-    cb(x)
+// async
+const async = f => {
+  return (x:number, cb:Function) => {
+    setTimeout( () => { 
+    cb(f(x))
     } , 1000);
+  }
+}
+const asyncAdd1 = async(add1);
+const asyncMul3 = async(mul3);
+asyncAdd1(1, r => UTILS.showResult('async_add1', r));
+asyncMul3(1, r => UTILS.showResult('async_Mul3', r));
+
+// Continious Passing Functoin
+const composeHOF = (f, g) => (x, cb) => {
+  f(x, r => g(r, r => cb(r)))
+}
+composeHOF(asyncAdd1, asyncMul3)(
+  4, r => UTILS.showResult('asyncAdd1ThenMul3CPS', r))
+
+
+// Exec
+const exec = f => x => ({
+  exec: cb => {
+    f(x, r => cb(r))
+  }
+});
+exec(asyncAdd1)(4).exec(r => UTILS.showResult('exec(asyncAdd1)(4)', r));
+exec(asyncMul3)(4).exec(r => UTILS.showResult('exec(asyncMul3)(4)', r));
+const asyncAdd1exec = exec(asyncAdd1);
+const asyncMul3exec = exec(asyncMul3);
+const composeCPS_HOF_EXEC = (f, g) => x => ({
+  exec: cb => {
+    f(x).exec( r => g(r).exec(cb) )
+  }
+})
+composeCPS_HOF_EXEC(asyncAdd1exec, asyncMul3exec)(4).exec(
+  r => UTILS.showResult('composeCPS_HOF_EXEC', r));
+
+
+// BindExec
+const bindExec = (execF, f) => ({
+  exec: cb => {
+    execF.exec( y => f(y).exec(cb))
+  }
+})
+bindExec(asyncAdd1exec(4), asyncMul3exec).exec(
+  r => UTILS.showResult('bindExec', r)
+);
+
+
+
+// Create object
+const createBindObject = exec => ({
+  exec,
+  bind(f) {
+    return createBindObject( cb => {
+      this.exec( r => f(r).exec(cb) )
+    })
+  } 
+})
+
+const toObject = f => x => {
+  return createBindObject( cb => {
+    f(x, cb)
+  })
 }
 
-asyncAdd1(1, r => console.log(r));
+const objectAsyncAdd1 = toObject(asyncAdd1);
+const objectAsyncMul3 = toObject(asyncMul3);
 
-// split ()()
+objectAsyncAdd1(4)
+.bind(objectAsyncMul3)
+.exec(
+  r => UTILS.showResult('createBindObject', r)
+)
 
-// creaete object 
 
-// add exec
 
-// use exec as first argument
+// pipe
+const createSubscribeObj = subs => ({
+  subs,
+  pipe(f) {
+    return createSubscribeObj(cb => {
+      this.subs( r => 
+        f(r).subs(cb))
+    })
+  }
+})
+const toSubObject = f => x => {
+  return createSubscribeObj( cb => {
+    f(x, cb)
+  })
+}
+const subObjectAsyncAdd1 = toSubObject(asyncAdd1);
+const subObjectAsyncMul3 = toSubObject(asyncMul3);
+subObjectAsyncAdd1(4)
+.pipe(subObjectAsyncMul3)
+.pipe(subObjectAsyncMul3)
+.subs(
+  r => UTILS.showResult('pipe', r)
+)
+
--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 
 // Show result
 export const showResult = (desc, text) => {
-  var node = document.createElement("span");                 
+  var node = document.createElement("li");                 
   var textnode = document.createTextNode(`${desc} =  ${text}`);
   node.appendChild(textnode);    
   document.getElementById("results").appendChild(node); 
